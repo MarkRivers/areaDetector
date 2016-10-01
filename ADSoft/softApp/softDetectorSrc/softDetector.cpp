15feab4a9eedab9df29d9a32ce433518bc9c15b2
David J. Vine <djvine@gmail.com>
2016-10-01 00:32:55 -0700

    Added ADSoft driver.

---
+++ ADSoft/softApp/softDetectorSrc/softDetector.cpp
@@ -0,0 +1,29 @@
+/* softDriver.h
+ * 
+ * This is a driver that allows any software to send images to area detector.
+ * 
+ * Author: David J. Vine
+ * Berkeley National Lab
+ * 
+ * Created: September 28, 2016
+ *
+ */
+
+
+#include <epicsEvent.h>
+#include "ADDriver.h"
+
+class epicsShareClass softDetector : public ADDriver {
+public:
+    softDetector(const char *portName,
+                 int maxBuffers, size_t maxMemory,
+                 int priority, int stackSize);
+
+};
+
+typedef enum {
+    Overwrite,
+    Append,
+}WriteModes_t;
+
+
