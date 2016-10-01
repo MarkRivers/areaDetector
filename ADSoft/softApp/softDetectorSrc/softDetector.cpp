ae5a6bb25142e218ff9e846db36210cf32215743
David J. Vine <djvine@gmail.com>
2016-10-01 01:09:30 -0700

    Finished wire framing.

--- ADSoft/softApp/softDetectorSrc/softDetector.cpp
+++ ADSoft/softApp/softDetectorSrc/softDetector.cpp
@@ -1,4 +1,4 @@
-/* softDriver.h
+/* softDriver.cpp
  * 
  * This is a driver that allows any software to send images to area detector.
  * 
@@ -9,21 +9,114 @@
  *
  */
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
+#include <epicsTime.h>
+#include <epicsThread.h>
 #include <epicsEvent.h>
+#include <epicsMutex.h>
+#include <epicsString.h>
+#include <epicsStdio.h>
+#include <cantProceed.h>
+#include <iocsh.h>
+
 #include "ADDriver.h"
+#include <epicsExport.h>
+#include "softDetector.h"
 
-class epicsShareClass softDetector : public ADDriver {
-public:
-    softDetector(const char *portName,
+static const char *driverName = "sofDetector";
+
+/** Constructor for softDetector; most parameters are simply passed to ADDriver::ADDriver.
+  * \param[in] portName The name of the asyn port to be created
+  * \param[in] maxBuffers The maximum number of NDArray buffers that the NDArrayPool for this driver
+  *            is allowed to allocate. Set this to -1 to allow an unlimited number of buffers.
+  * \param[in] maxMemory The maximum amount of memory that the NDArrayPool for this driver is 
+  *            is allowed to allocate. Set this to -1 to allow an unlimited amount of memory.
+  * \param[in] priority The thread priority for the asyn port driver thread if ASYN_CANBLOCK is set
+  *            in asynFlags.
+  * \param[in] stackSize The stack size for the asyn port driver thread if ASYN_CANBLOCK is set in
+  *            asynFlags.
+  **/
+
+softDetector::softDetector(const char *portName,
                  int maxBuffers, size_t maxMemory,
-                 int priority, int stackSize);
+                 int priority, int stackSize)
 
-};
+    : ADDriver(portName,
+               maxBuffers, maxMemory,
+               0, 0, /* No interface beyond those defined in ADDriver.cpp */
+               0, 1, /* ASYN_CANBLOCK=0, ASYN_MULTIDEVICE=0, autoConnect=1 */
+               priority, stackSize),
+               pRaw(NULL)
 
-typedef enum {
-    Overwrite,
-    Append,
-}WriteModes_t;
+{
+    int status asynSuccess;
+    const char *functionName = "softDetector";
 
+    status  = setStringParam (ADManufacturer, "Soft Detector");
+    status |= setStringParam (ADModel, "Software Detector");
+    status |= setIntegerParam(ADMaxSizeX, 1000);
+    status |= setIntegerParam(ADMaxSizeY, 1000);
+    status |= setIntegerParam(ADMinX, 0); 
+    status |= setIntegerParam(ADMinY, 0); 
+    status |= setIntegerParam(ADBinX, 1); 
+    status |= setIntegerParam(ADBinY, 1); 
+    status |= setIntegerParam(ADReverseX, 0); 
+    status |= setIntegerParam(ADReverseY, 0); 
+    status |= setIntegerParam(ADSizeX, 1000);
+    status |= setIntegerParam(ADSizeY, 1000);
+    status |= setIntegerParam(NDArraySizeX, 1000);
+    status |= setIntegerParam(NDArraySizeY, 1000);
+    status |= setIntegerParam(NDArraySize, 0); 
+    status |= setIntegerParam(ADImageMode, ADImageContinuous);
+    status |= setDoubleParam (ADAcquireTime, .001);
+    status |= setDoubleParam (ADAcquirePeriod, .005);
+    status |= setIntegerParam(ADNumImages, 100);
+
+    if (status) {
+	printf("%s: Unable to set camera prameters.", functionName);
+	return;
+    }
+}
+extern "C" int softDetectorConfig(const char *portName, int maxBuffers, int maxMemory, int priority, int stackSize)
+{
+    new softDetector(portName,
+                    (maxBuffers < 0) ? 0 : maxBuffers,
+                    (maxMemory < 0) ? 0 : maxMemory,
+                    priority, stackSize);
+    return(asynSuccess);
+}
+
+/** Code for iocsh registration */
+static const iocshArg softDetectorConfigArg0 = {"Port name", iocshArgString};
+static const iocshArg softDetectorConfigArg1 = {"maxBuffers", iocshArgInt};
+static const iocshArg softDetectorConfigArg2 = {"maxMemory", iocshArgInt};
+static const iocshArg softDetectorConfigArg3 = {"priority", iocshArgInt};
+static const iocshArg softDetectorConfigArg4 = {"stackSize", iocshArgInt};
+static const iocshArg * const softDetectorConfigArgs[] =  {&softDetectorConfigArg0,
+                                                           &softDetectorConfigArg1,
+                                                           &softDetectorConfigArg2,
+                                                           &softDetectorConfigArg3,
+                                                           &softDetectorConfigArg4};
+static const iocshFuncDef configsoftDetector = {"softDetectorConfig", 5, softDetectorConfigArgs};
+static void configsoftDetectorCallFunc(const iocshArgBuf *args)
+{
+    softDetectorConfig(args[0].sval, args[1].ival, args[2].ival, args[3].ival, args[4].ival);
+}
+
+static void softDetectorRegister(void)
+{
+
+    iocshRegister(&configsoftDetector, configsoftDetectorCallFunc);
+}
+
+extern "C" {
+epicsExportRegistrar(softDetectorRegister);
+}
 
